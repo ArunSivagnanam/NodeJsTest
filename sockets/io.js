e29708bb26ee4f5e4cecae2a427fc005aca62516
Arun Sivagnanam <arun.s@live.dk>
2015-10-20 23:45:20 +0200

    added cluster io

--- sockets/io.js
+++ sockets/io.js
@@ -6,6 +6,9 @@ module.exports = function(socketIO){
 
         socketIO.sockets.on('connection',function(socket){
 
+
+
+
             socket.on('send-message',function(data){
                 socketIO.sockets.emit('new-message',data);
             });
